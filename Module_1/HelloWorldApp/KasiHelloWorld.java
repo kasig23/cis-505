9dd8458c5bf4e4f04febd1ad9b96d67701647ddd
Kasi Viswanath <kgorige@my365.bellevue.edu>
2022-09-02 05:48:59 +0530

    module_1 task

---
+++ Module_1/HelloWorldApp/KasiHelloWorld.java
@@ -0,0 +1,8 @@
+public class KasiHelloWorld {
+
+	private static String firstName="Kasi";
+	private static String lastName="Viswanath";
+	public static void main(String[] args) {
+		System.out.println(String.format("My name is %s %s", firstName, lastName));
+	}// end main
+}// end KasiHelloWorld
