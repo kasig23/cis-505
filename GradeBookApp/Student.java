293142b73f79f17711814d5c9b9212d2de095bf2
Kasi Viswanath <kgorige@my365.bellevue.edu>
2022-11-16 23:13:46 -0600

    added student class

---
+++ GradeBookApp/Student.java
@@ -0,0 +1,81 @@
+package com.java8.module10;
+
+public class Student {
+
+	private String firstName;
+	
+	private String lastName;
+	
+	private String course;
+	
+	private String grade;
+
+	public Student() {
+		
+	}
+	public Student(String firstName, String lastName, String course, String grade) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.course = course;
+		this.grade = grade;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the course
+	 */
+	public String getCourse() {
+		return course;
+	}
+
+	/**
+	 * @param course the course to set
+	 */
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	/**
+	 * @return the grade
+	 */
+	public String getGrade() {
+		return grade;
+	}
+
+	/**
+	 * @param grade the grade to set
+	 */
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+	
+}
